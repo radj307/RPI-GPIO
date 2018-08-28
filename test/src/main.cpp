002aa1e14bdaa19b8c70dc48796fec9e674986e4
Guillaume Bauer <guillaume.bauer.418@gmail.com>
2018-08-28 15:28:05 +0200

    Testing GPIO instanciation

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -1,7 +1,10 @@
+#include <iostream>
 #include <RPI-GPIO/RPI-GPIO.h>
 
-int main(int argc, char const *argv[])
-{
-    printHelloWorld();
+int main(int argc, char const *argv[]) {
+    GPIO gpio{};
+    if (!gpio.connect()) {
+        std::cout << "Couldn't connect to GPIO\n";
+    }
     return 0;
 }
