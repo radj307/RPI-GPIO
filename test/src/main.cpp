c1ecfea44261915bc39426e92ef59dcd77634052
Guillaume Bauer <guillaume.bauer.418@gmail.com>
2018-09-03 11:24:47 +0200

    Added tests

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <unistd.h>
 #include <RPI-GPIO/RPI-GPIO.h>
 
 int main(int argc, char const *argv[]) {
@@ -7,6 +8,17 @@ int main(int argc, char const *argv[]) {
         gpio.connect();
     } catch (Exception e) {
         std::cout << e.what() << "\nError code : " << e.error() << "\n";
+        return 1;
+    }
+
+    const char pin = 2;
+
+    gpio.pinMode(pin, PIN_MODE::OUTPUT);
+    for (int i = 0; i < 5; i++) {
+        gpio.pinDown(pin);
+        sleep(1);
+        gpio.pinUp(pin);
+        sleep(1);
     }
     return 0;
 }
